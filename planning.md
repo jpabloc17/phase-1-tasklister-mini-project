0cfc596951da221610c5d1cae7eda245ced0cb3f
Pablo Castro <pablocas-tro1@outlook.com>
2023-04-15 21:07:25 -0400

    display task

---
+++ planning.md
@@ -0,0 +1,4 @@
+stage 1 
+[x] get the form and add an event listener to the form
+[x] get the task from the input
+[x] display the data
--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  addingEventListeners()
 });
+
+// get the form and add an event listener to the form
+function addingEventListeners() {
+  document.getElementById("create-task-form").addEventListener("submit", handleFormSubmit)
+}
+
+function handleFormSubmit(e){
+  e.preventDefault()
+  const task = e.target[0].value
+  // console.log(task)
+
+  displayTask(task)
+}
+
+function displayTask(task){
+  const taskHoldUl = document.getElementById("tasks")
+  const taskHoldLi = document.createElement("li")
+
+  taskHoldLi.textContent = task
+  taskHoldUl.appendChild(taskHoldLi)
+  // console.log(taskHoldLi)
+}
